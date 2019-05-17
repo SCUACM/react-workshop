ab94c2a1bc0218d8870f8c7a0f440818bed33406
Tim Shur <tim.shur@gmail.com>
2019-05-17 14:52:50 -0700

    Feed: add subreddit search and error reporting

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -9,8 +9,13 @@ class Feed extends Component {
         super(props);
 
         this.state = {
-            posts: []
+            posts: [],
+            subreddit: '/r/aww',
+            error: ''
         };
+
+        this.handleSubredditChange = this.handleSubredditChange.bind(this);
+        this.handleSubredditSubmit = this.handleSubredditSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -25,19 +30,60 @@ class Feed extends Component {
             });
     }
 
-    render() {
-        const { posts } = this.state;
+    handleSubredditChange(event) {
+        this.setState({
+            subreddit: event.target.value
+        });
+    }
 
-        console.log(posts);
+    handleSubredditSubmit(event) {
+        const { subreddit } = this.state;
+
+        if (subreddit === '')
+            return;
+
+        if (event.key === 'Enter') {
+            axios.get('https://www.reddit.com' + subreddit + '/hot.json')
+                .then(result => {
+                    this.setState({
+                        posts: result.data.data.children,
+                        error: false
+                    });
+                })
+                .catch(error => {
+                    this.setState({
+                        posts: [],
+                        error: true
+                    })
+                });
+        }
+    }
+
+    render() {
+        const { posts, subreddit, error } = this.state;
 
         return (
             <div className='container'>
-                <h1 className='feed-title'>React Reddit Feed</h1>
-                <div className='feed'>
-                    {posts.map((post, index) =>
-                        <FeedItem post={post.data} key={index} />
-                    )}
+                <div className='header'>
+                    <h1 className='feed-title'>React Reddit Feed</h1>
+                    <input
+                        className='subreddit-input'
+                        placeholder='enter a subreddit'
+                        value={subreddit}
+                        onChange={this.handleSubredditChange}
+                        onKeyDown={this.handleSubredditSubmit}
+                    />
                 </div>
+                {error === ''
+                 ?  <div className='feed'>
+                        {posts.map((post, index) =>
+                            <FeedItem post={post.data} key={index} />
+                        )}
+                    </div>
+                 :  <div className='error'>
+                        Error: Subreddit doesn't exist or something went wrong!
+                    </div>
+                }
             </div>
         );
     }
