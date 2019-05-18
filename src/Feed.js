fd4e4b9666bb3ff3cd358a2697aff444799ba4c4
Tim Shur <tim.shur@gmail.com>
2019-05-17 19:58:04 -0700

    create workshop template

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 
 import FeedItem from './FeedItem';
 
@@ -8,58 +7,11 @@ class Feed extends React.Component {
         super(props);
 
         this.state = {
-            posts: [],
-            value: 'aww',
-            error: ''
+
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleKeyDown = this.handleKeyDown.bind(this);
-    }
-
-    componentDidMount() {
-        axios.get('https://www.reddit.com/r/aww/hot.json?')
-            .then(result => {
-                this.setState({
-                    posts: result.data.data.children
-                });
-            })
-            .catch(error => console.log(error));
-    }
-
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        });
-    }
-
-    handleKeyDown(event) {
-        const { value } = this.state;
-
-        if (value === '')
-            return;
-
-        if (event.key === 'Enter') {
-            axios.get('https://www.reddit.com/r/' + value + '/hot.json?')
-                .then(result => {
-                    this.setState({
-                        posts: result.data.data.children,
-                        error: ''
-                    });
-                })
-                .catch(error => {
-                    console.log(error);
-                    this.setState({
-                        posts: [],
-                        error: true
-                    })
-                });
-        }
     }
 
     render() {
-        const { posts, value, error } = this.state;
-
         return (
             <div className='container'>
                 <div className='header'>
@@ -67,20 +19,11 @@ class Feed extends React.Component {
                     <div className='subreddit-prefix'>/r/</div>
                     <input
                         className='subreddit-input'
-                        placeholder='enter a subreddit'
-                        value={value}
-                        onChange={this.handleChange}
-                        onKeyDown={this.handleKeyDown}
                     />
                 </div>
-                {error === ''
-                 ?  <div className='feed'>
-                        {posts.map((post, index) => <FeedItem post={post.data} key={index} />)}
-                    </div>
-                 :  <div className='error'>
-                        Error: Subreddit doesn't exist or something went wrong!
-                    </div>
-                }
+                <div className='feed'>
+                    <FeedItem />
+                </div>
             </div>
         );
     }
--- src/FeedItem.js
+++ src/FeedItem.js
@@ -5,52 +5,22 @@ class FeedItem extends React.Component {
     super(props);
 
     this.state = {
-      votes: props.post.ups
+
     };
-
-    this.handleUpvote = this.handleUpvote.bind(this);
-    this.handleDownvote = this.handleDownvote.bind(this);
-    this.handlePostClick = this.handlePostClick.bind(this);
-  }
-
-  handleUpvote() {
-    const { votes } = this.state;
-
-    this.setState({
-      votes: votes + 1
-    });
-  }
-
-  handleDownvote() {
-    const { votes } = this.state;
-
-    this.setState({
-      votes: votes - 1
-    });
-  }
-
-  handlePostClick(event) {
-    const { post } = this.props;
-
-    event.preventDefault();
-    window.open('https://www.reddit.com' + post.permalink, '_blank');
   }
 
   render() {
-    const { votes } = this.state;
-    const { post } = this.props;
-
     return (
       <div className='feed-item card'>
         <div className='votes'>
-          <div className='vote upvote' onClick={this.handleUpvote}>+</div>
-          <div>{votes}</div>
-          <div className='vote downvote' onClick={this.handleDownvote}>-</div>
+          <div className='vote upvote'>+</div>
+          <div></div>
+          <div className='vote downvote'>-</div>
         </div>
-        <div className='post' onClick={this.handlePostClick}>
-          <div className='post-info'>r/{post.subreddit}</div>
-          <div className='title'>{post.title}</div>
-          <img src={post.thumbnail} alt=''></img>
+        <div className='post'>
+          <div className='post-info'>r/aww</div>
+          <div className='title'>Hello world!</div>
+          <img src='https://bit.ly/2W9oYlO' alt=''></img>
         </div>
       </div>
     );
